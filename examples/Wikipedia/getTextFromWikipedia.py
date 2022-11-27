1b74197709b4b99dbcbbffd19899da91b9d4e145
danigsp12 <109767633+danigsp12@users.noreply.github.com>
2022-11-26 23:44:57 -0300

    tu cochinah ahí

--- examples/Wikipedia/getTextFromWikipedia.py
+++ examples/Wikipedia/getTextFromWikipedia.py
@@ -1,5 +1,25 @@
 import wikipedia as wiki
 
-a = wiki.page("Inquisición")
+wiki.set_lang("es")
 
-print(a.content)
+a = wiki.page("Anime")
+b = wiki.page("One Piece")
+c = wiki.page("Liverpool FC")
+d = wiki.page("Ryzen")
+e = wiki.page("Pisco Elqui")
+f = wiki.page("Cerveza")
+g = wiki.page("Ron")
+h = wiki.page("Pokemon")
+i = wiki.page("Digimon")
+j = wiki.page("Condorito")
+
+print(a.summary)
+print(b.summary)
+print(c.summary)
+print(d.summary)
+print(e.summary)
+print(f.summary)
+print(g.summary)
+print(h.summary)
+print(i.summary)
+print(j.summary)
