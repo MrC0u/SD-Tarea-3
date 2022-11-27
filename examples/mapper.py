eb3312b1ea6c589aa29d254b097d96e273292556
BuildTools <marcosdvalderr@hotmail.com>
2022-11-27 03:26:50 -0300

    -

--- examples/mapper.py
+++ examples/mapper.py
@@ -4,9 +4,16 @@
 import sys
 import re
 
+aux = 1
+
 for line in sys.stdin:
     line = re.sub(r'\W+',' ',line.strip())
     words = line.split()
 
+    
     for word in words:
-        print('{}\t{}'.format(word,1))
+        if word == 'DocumentEnd':
+            aux += 1
+            continue
+        print('{}\t{}'.format(word,aux))
+
--- examples/reducer.py
+++ examples/reducer.py
@@ -5,24 +5,39 @@ import sys
 
 current_word = None
 current_count = 0
+current_doc = 0
+current = 0
 word = None
 
 for line in sys.stdin:
     line = line.strip()
-    word, count = line.split('\t',1)
+    word, doc = line.split('\t',1)
+    count = 1
 
     try:
-        count = int(count)
+        doc = int(doc)
     except ValueError:
         continue
-
     if current_word == word:
-        current_count += count
+        if current_doc == doc:
+            amount[current] = ('('+str(current_count)+','+ str(current_doc) +')')
+            current_count += count
+        else:
+            current_doc = doc
+            current_count = count
+            current += 1
+            amount.append('('+str(current_count)+','+ str(current_doc) +')')
+            current_count += 1
     else:
         if current_word:
-            print('{}\t{}'.format(current_word,current_count))
+            print('{}\t{}'.format(current_word,amount))
         current_word = word
         current_count = count
+        current_doc = doc
+        current = 0
+        amount = []
+        amount.append('('+str(current_count)+','+ str(current_doc) +')')
+        #print(amount)
 
 
 if current_word == word:
