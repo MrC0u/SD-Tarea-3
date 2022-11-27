c6fd19ea3a84243f971f84e78e5b79689a23b316
BuildTools <marcosdvalderr@hotmail.com>
2022-11-27 19:42:24 -0300

    -

--- examples/Wikipedia/getTextFromWikipedia.py
+++ examples/Wikipedia/getTextFromWikipedia.py
@@ -1,25 +1,31 @@
+# Importar API de Wikipedia
 import wikipedia as wiki
 
+# Cambiamos el lenguaje en el que vamos a buscar
 wiki.set_lang("es")
 
-datos = [wiki.page("Anime"), wiki.page("One Piece"), wiki.page("Liverpool FC"), 
-wiki.page("Ryzen"), wiki.page("Pisco Elqui"), wiki.page("Cerveza"), wiki.page("Ron"), wiki.page("Pokemon"), 
-wiki.page("Digimon"), wiki.page("Condorito")]
+# Hacemos un arreglo con las busquedas que deseamos realizar 
+busquedas = ["Anime","One Piece","Liverpool FC", "Ryzen", "Pisco Elqui", "Cerveza", "Ron", "Pokemon", "Digimon", "Condorito"]
 
-data1 = open("./Output/data1.txt", "w", encoding="utf-8")
-data3 = open("./Output/data3.txt", "w", encoding="utf-8")
-data4 = open("./Output/data4.txt", "w", encoding="utf-8")
-data2 = open("./Output/data2.txt", "w", encoding="utf-8")
-data5 = open("./Output/data5.txt", "w", encoding="utf-8")
-data6 = open("./Output/data6.txt", "w", encoding="utf-8")
-data7 = open("./Output/data7.txt", "w", encoding="utf-8")
-data8 = open("./Output/data8.txt", "w", encoding="utf-8")
-data9 = open("./Output/data9.txt", "w", encoding="utf-8")
-data10 = open("./Output/data10.txt", "w", encoding="utf-8")
+# Creamos arreglos para guardar los archivos y la data
+datos = []
+data = []
 
-output = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10]
+print('Descargando data...')
 
-for i in range(len(output)):
-    output[i].write(str(i+1)+'DocumentBegin'+'\n')
-    output[i].write(datos[i].content)
-    output[i].close()
+# Guardamos la data en un arreglo y creamos los archivos de salida 
+for i in range(len(busquedas)):
+    #print(i)
+    datos.append(wiki.page(busquedas[i]))
+    data.append(open("./Output/data"+str(i+1)+".txt", "w", encoding="utf-8"))
+
+# Guardamos la data en los archivos de salida
+for i in range(len(data)):
+    print('Escribiendo data'+str(i+1)+'.txt')
+    # Insertamos un numero de documento + un "DocumentBegin" que utilizaremos luego para el procesamiento del mapper
+    data[i].write(str(i+1)+'DocumentBegin'+'\n')
+    # Luego insertamos el contenido de la busqueda
+    data[i].write(datos[i].content)
+    data[i].close()
+
+print('Escritura Finalizada')
