37da3b4a1c38a8495ec7dd22648b4bee8b666264
danigsp12 <109767633+danigsp12@users.noreply.github.com>
2022-11-27 00:08:23 -0300

    pythoncito automata

--- examples/Wikipedia/getTextFromWikipedia.py
+++ examples/Wikipedia/getTextFromWikipedia.py
@@ -2,24 +2,23 @@ import wikipedia as wiki
 
 wiki.set_lang("es")
 
-a = wiki.page("Anime")
-b = wiki.page("One Piece")
-c = wiki.page("Liverpool FC")
-d = wiki.page("Ryzen")
-e = wiki.page("Pisco Elqui")
-f = wiki.page("Cerveza")
-g = wiki.page("Ron")
-h = wiki.page("Pokemon")
-i = wiki.page("Digimon")
-j = wiki.page("Condorito")
+datos = [wiki.page("Anime"), wiki.page("One Piece"), wiki.page("Liverpool FC"), 
+wiki.page("Ryzen"), wiki.page("Pisco Elqui"), wiki.page("Cerveza"), wiki.page("Ron"), wiki.page("Pokemon"), 
+wiki.page("Digimon"), wiki.page("Condorito")]
 
-print(a.summary)
-print(b.summary)
-print(c.summary)
-print(d.summary)
-print(e.summary)
-print(f.summary)
-print(g.summary)
-print(h.summary)
-print(i.summary)
-print(j.summary)
+data1 = open("data1.txt", "w", encoding="utf-8")
+data2 = open("data2.txt", "w", encoding="utf-8")
+data3 = open("data3.txt", "w", encoding="utf-8")
+data4 = open("data4.txt", "w", encoding="utf-8")
+data5 = open("data5.txt", "w", encoding="utf-8")
+data6 = open("data6.txt", "w", encoding="utf-8")
+data7 = open("data7.txt", "w", encoding="utf-8")
+data8 = open("data8.txt", "w", encoding="utf-8")
+data9 = open("data9.txt", "w", encoding="utf-8")
+data10 = open("data10.txt", "w", encoding="utf-8")
+
+output = [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10]
+
+for i in range(len(output)):
+    output[i].write(datos[i].content)
+    output[i].close()
